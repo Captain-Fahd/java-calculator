31dec75999396bfa0e7ffd2f5f519579a3a1b71f
Captain-Fahd <omarfahd06@gmail.com>
2025-04-30 23:52:34 +1000

    Added Operations class & minor logic implementation in CalcView  - Added an operations class that handles the addition, subtraction etc.  - Created button variables along with some other variables required for logic implementation.

--- JavaCalculator/src/CalculatorView.java
+++ JavaCalculator/src/CalculatorView.java
@@ -1,5 +1,22 @@
-import java.awt.*;
 import javax.swing.*;
-public class CalculatorView {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class CalculatorView extends JFrame implements ActionListener {
+    private String operation;
+    private JButton Addition, Subtraction, Multiplication, Division, Equals, Clear;
+    private Boolean isExpression;
+
+
+    CalculatorView(){
+        setTitle("Calculator");
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
--- JavaCalculator/src/Main.java
+++ JavaCalculator/src/Main.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+       CalculatorView view = new CalculatorView();
+       view.setVisible(true);
+       view.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
